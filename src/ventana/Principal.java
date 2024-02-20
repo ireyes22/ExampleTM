ddd6e116cb48a7bf5beb5b2d07721622aa731c51
ireyes22 <159938778+ireyes22@users.noreply.github.com>
2024-02-20 09:59:04 -0700

    unidad 2: ejercicio 13

---
+++ src/ventana/Principal.java
@@ -0,0 +1,12 @@
+package ventana;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//crea la ventana
+		Ventana ventana = new Ventana();
+	}
+
+}
