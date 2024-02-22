34d5c053d2da4caa97e66d95d19e5819856ec0a7
ireyes22 <159938778+ireyes22@users.noreply.github.com>
2024-02-22 08:35:25 -0700

    unidad 2: ejercicio 19

--- src/Ventana.java
+++ src/Ventana.java
@@ -2,12 +2,22 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 
+import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
+import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class Ventana extends JFrame{
@@ -19,7 +29,7 @@ public class Ventana extends JFrame{
 		//this.setVisible(true);
 		
 		//tamaño de la ventana
-		this.setSize(1000, 500);
+		this.setSize(1000,750);
 		
 		//cerrar ventana por defecto
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -45,10 +55,25 @@ public class Ventana extends JFrame{
 		this.iniciarComponentes();
 		
 		//hace visible la pantalla
-				this.setVisible(true);
+		this.setVisible(true);
 	}
 	
 	public void iniciarComponentes() {
+		
+		//this.login();
+		//this.registro();//añadir el panel de registro
+		this.admin();
+		
+		//size
+				//location
+				//background-opaque
+				//string cosntructor
+		
+		this.repaint();
+		this.validate();
+	}
+	
+	public void login() {
 		
 		JPanel login = new JPanel();
 		login.setSize(this.getWidth()/2, this.getHeight());//el panel aparece a la mitad del frame
@@ -96,18 +121,223 @@ public class Ventana extends JFrame{
 		accept.setBounds(50, 340, 200, 70);
 		login.add(accept);
 		
-		//size
-		//location
-		//background-opaque
-		//string cosntructor
+		this.add(login);//añadir el panel de login
+	}
+	
+	public void registro() {
 		
 		JPanel registro = new JPanel();//crear otro panel
 		registro.setSize(this.getWidth()/2, this.getHeight());
-		registro.setBackground(new Color(170, 217, 187));//añade color en RGB
+		registro.setBackground(Color.decode("#9195F6"));//añade color en RGB
 		registro.setLocation(500,0);//el panel aparece en la otra mitad del frame
+		registro.setLayout(null);
 		
-		this.add(login);//añadir el panel de login
-		//this.add(registro);//añadir el panel de registro
+		JLabel login_title = new JLabel("REGISTRO",0);
+		login_title.setSize(300, 80);//tamaño del texto
+		login_title.setFont(new Font("Comic Sans MS",Font.BOLD,38));//establece fuente del texto
+		login_title.setForeground(Color.white);//cambiar color al texto
+		login_title.setLocation(100,20);//añade en donde localizar el componente
+		login_title.setOpaque(true);//agrega fondo al texto
+		login_title.setBackground(Color.BLACK);//color al fondo del texto
+		registro.add(login_title);
 		
+		JLabel usuario = new JLabel("NOMBRE DE USUARIO: ",0);
+		usuario.setBounds(50,120,400,40);//(x,y,ancho,alto)
+		usuario.setFont(new Font("Comic Sans MS",Font.BOLD,20));//establece fuente del texto
+		usuario.setOpaque(true);//hace visible el color del fondo
+		usuario.setBackground(Color.yellow);//color del fondo amarillo
+		registro.add(usuario);//añade el componente
+		
+		JTextField name_text = new JTextField();
+		name_text.setBounds(50, 160, 400, 40); // Ajustar las coordenadas y tamaño del campo de texto
+		name_text.setHorizontalAlignment(0);//alinear al centro
+		name_text.setFont(new Font("Comic Sans MS",Font.ITALIC,17));
+	    registro.add(name_text);
+	    
+	    JLabel bio = new JLabel("BIOGRAFIA",0);
+	    bio.setBounds(50, 200, 400, 40);
+	    bio.setFont(new Font("Comic Sans MS",Font.BOLD,20));//establece fuente del texto
+	    registro.add(bio);//añade el componente
+	    
+	    JTextArea bio_text = new JTextArea();//escribir varias lineas de texto
+	    bio_text.setBounds(50, 240, 400, 80);
+	    bio_text.setFont(new Font("Comic Sans MS",Font.ITALIC,17));
+	    registro.add(bio_text);
+	    
+	    JLabel prefer = new JLabel("PREFERENCIAS",0);
+	    prefer.setBounds(50, 320, 400, 40);
+	    prefer.setFont(new Font("Comic Sans MS",Font.BOLD,20));//establece fuente del texto
+	    registro.add(prefer);//añade el componente
+	    
+		JCheckBox item_box1=new JCheckBox("Dulce");
+		item_box1.setBounds(50, 360, 80, 40);
+		item_box1.setFont(new Font("Comic Sans MS",Font.BOLD,20));//establece fuente del texto
+		item_box1.setOpaque(false);
+		//item_box1.setBackground(Color.red);
+		registro.add(item_box1);
+		
+		JCheckBox item_box2=new JCheckBox("Salado");
+		item_box2.setBounds(140, 360, 90, 40);
+		item_box2.setFont(new Font("Comic Sans MS",Font.BOLD,20));
+		item_box2.setOpaque(false);
+		//item_box2.setBackground(Color.red);
+		registro.add(item_box2);
+	    
+		JCheckBox item_box3=new JCheckBox("Saludable");
+		item_box3.setBounds(240, 360, 120, 40);
+		item_box3.setFont(new Font("Comic Sans MS",Font.BOLD,20));
+		item_box3.setOpaque(false);
+		//item_box3.setBackground(Color.red);
+		item_box3.setBorderPainted(true);//agregar bordes
+		item_box3.setBorder(BorderFactory.createLineBorder(Color.blue,2));//pintar bordes 
+		registro.add(item_box3);
+		
+		 JLabel terminos = new JLabel("TERMINOS",0);
+	    terminos.setBounds(50, 400, 400, 40);
+	    terminos.setFont(new Font("Comic Sans MS",Font.BOLD,20));
+	    terminos.setOpaque(true);
+		terminos.setBackground(Color.yellow);
+	    registro.add(terminos);
+	    
+	    JRadioButton acceptRadio = new JRadioButton("Acepto los terminos");
+	    acceptRadio.setBounds(50, 440, 200, 40);
+	    acceptRadio.setFont(new Font("Comic Sans MS",Font.BOLD,15));
+	    acceptRadio.setOpaque(false);
+	    registro.add(acceptRadio);
+	    
+	    JRadioButton noAcceptRadio = new JRadioButton("No acepto los terminos");
+	    noAcceptRadio.setBounds(250, 440, 200, 40);
+	    noAcceptRadio.setFont(new Font("Comic Sans MS",Font.BOLD,15));
+	    noAcceptRadio.setOpaque(false);
+	    registro.add(noAcceptRadio);
+	    
+	    ButtonGroup terms = new ButtonGroup();//agrupa los radios buttons
+	    terms.add(acceptRadio);
+	    terms.add(noAcceptRadio);
+	    
+	    String colonia[]= {"Centro", "Camino Real","Pedregal","Conchalito","La 8"};
+	    JComboBox location = new JComboBox(colonia);
+	    location.setBounds(50, 480, 400, 40);
+	    location.setFont(new Font("Comic Sans MS",Font.BOLD,20));
+	    registro.add(location);
+	    
+	    JButton accept = new JButton("Crear Cuenta");
+		accept.setFont(new Font("Comic Sans MS",Font.BOLD,28));//establece fuente del texto
+		accept.setBounds(50, 530, 400, 70);
+		registro.add(accept);
+		
+		this.add(registro);//añadir el panel de registro
+	}
+	
+	public void admin() {
+		//añadir panel
+		JPanel adminPanel = new JPanel();//crear otro panel
+		adminPanel.setSize(this.getWidth(), this.getHeight());
+		adminPanel.setBackground(Color.decode("#BDCDD6"));//añade color en RGB
+		adminPanel.setLocation(0,0);//el panel aparece en la otra mitad del frame
+		adminPanel.setLayout(null);//quitar el molde para modificar
+		
+		//añadir menu
+		JMenuBar barra = new JMenuBar();//crea menu padre
+		
+		JMenu menuFile = new JMenu("Archivo");//añade un menu
+		
+		JMenuItem openItem = new JMenuItem("Abrir archivo");//añade submenus al menu
+		JMenuItem createItem = new JMenuItem("Crear archivo");
+		
+		barra.add(menuFile);//relaciona los menus (los junta)
+		menuFile.add(openItem);
+		menuFile.add(createItem);
+		
+		this.setJMenuBar(barra);
+		
+		//titulo
+		JLabel login_title = new JLabel("USUARIOS",0);
+		login_title.setSize(300, 80);//tamaño del texto
+		login_title.setFont(new Font("Comic Sans MS",Font.BOLD,38));//establece fuente del texto
+		login_title.setForeground(Color.white);//cambiar color al texto
+		login_title.setLocation(350,20);//añade en donde localizar el componente
+		login_title.setOpaque(true);//agrega fondo al texto
+		login_title.setBackground(Color.BLACK);//color al fondo del texto
+		adminPanel.add(login_title);
+		
+		/*JLabel widget = new JLabel();
+		widget.setBounds(40,120,300,100);
+		widget.setOpaque(true);
+		widget.setBackground(Color.BLACK);
+		adminPanel.add(widget);*/
+		
+		JLabel title_widget = new JLabel("Total de usuarios",0);
+		title_widget.setFont(new Font("Comic Sans MS",Font.BOLD,20));//establece fuente del texto
+		title_widget.setBounds(40,120,300,40);
+		title_widget.setForeground(Color.white);
+		adminPanel.add(title_widget);
+		
+		JLabel number_widget = new JLabel("100",0);
+		number_widget.setFont(new Font("Comic Sans MS",Font.BOLD,20));//establece fuente del texto
+		number_widget.setBounds(40,160,300,40);
+		number_widget.setForeground(Color.white);
+		adminPanel.add(number_widget);
+		
+		JLabel widget = new JLabel();
+		widget.setBounds(40,120,300,100);
+		widget.setOpaque(true);
+		widget.setBackground(Color.BLACK);
+		adminPanel.add(widget);
+		
+		//botones
+		JButton descargar = new JButton("Exportar");
+		descargar.setBounds(750,230,100,40);
+		descargar.setOpaque(true);
+		descargar.setBackground(Color.white);
+		adminPanel.add(descargar);
+		
+		JButton addUser = new JButton("Añadir");
+		addUser.setBounds(860,230,100,40);
+		addUser.setOpaque(true);
+		addUser.setBackground(Color.white);
+		adminPanel.add(addUser);
+		
+		//añadir tabla
+		
+		String title[]= {"No.Control","Nombre","Apellidos","Semestre","Promedio","Acciones"};
+		String tableData[][]= {
+				{"No.Control","Nombre","Apellidos","Semestre","Promedio","Acciones"},
+				{"No.Control","Nombre","Apellidos","Semestre","Promedio","Acciones"},
+				{"No.Control","Nombre","Apellidos","Semestre","Promedio","Acciones"},
+				{"No.Control","Nombre","Apellidos","Semestre","Promedio","Acciones"},
+				{"No.Control","Nombre","Apellidos","Semestre","Promedio","Acciones"},
+				{"No.Control","Nombre","Apellidos","Semestre","Promedio","Acciones"},
+				{"No.Control","Nombre","Apellidos","Semestre","Promedio","Acciones"},
+				{"No.Control","Nombre","Apellidos","Semestre","Promedio","Acciones"},
+				{"No.Control","Nombre","Apellidos","Semestre","Promedio","Acciones"},
+				{"No.Control","Nombre","Apellidos","Semestre","Promedio","Acciones"},
+				{"No.Control","Nombre","Apellidos","Semestre","Promedio","Acciones"},
+				{"No.Control","Nombre","Apellidos","Semestre","Promedio","Acciones"},
+				{"No.Control","Nombre","Apellidos","Semestre","Promedio","Acciones"},
+				{"No.Control","Nombre","Apellidos","Semestre","Promedio","Acciones"},
+				{"No.Control","Nombre","Apellidos","Semestre","Promedio","Acciones"},
+				{"No.Control","Nombre","Apellidos","Semestre","Promedio","Acciones"},
+				{"No.Control","Nombre","Apellidos","Semestre","Promedio","Acciones"},
+				{"No.Control","Nombre","Apellidos","Semestre","Promedio","Acciones"},
+				{"No.Control","Nombre","Apellidos","Semestre","Promedio","Acciones"},
+				{"No.Control","Nombre","Apellidos","Semestre","Promedio","Acciones"},
+				{"No.Control","Nombre","Apellidos","Semestre","Promedio","Acciones"},
+				{"No.Control","Nombre","Apellidos","Semestre","Promedio","Acciones"},
+				{"No.Control","Nombre","Apellidos","Semestre","Promedio","Acciones"},
+				{"No.Control","Nombre","Apellidos","Semestre","Promedio","Acciones"},
+				{"No.Control","Nombre","Apellidos","Semestre","Promedio","Acciones"},
+				{"No.Control","Nombre","Apellidos","Semestre","Promedio","Acciones"},
+				{"No.Control","Nombre","Apellidos","Semestre","Promedio","Acciones"},
+				{"No.Control","Nombre","Apellidos","Semestre","Promedio","Acciones"}
+		};
+		
+		JTable tablaUser = new JTable(tableData,title);
+		//tablaUser.setBounds(40,280,920,300);
+		JScrollPane tableScroll = new JScrollPane(tablaUser);
+		tableScroll.setBounds(40,280,920,300);
+		adminPanel.add(tableScroll);
+		
+		this.add(adminPanel);//añade el nuevo panel al frame
 	}
 }
